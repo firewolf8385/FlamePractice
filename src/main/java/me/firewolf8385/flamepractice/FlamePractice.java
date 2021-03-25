1aafcba2f43248667baed35cec47be9bb77c30e8
colby <colbykrawczyk10@gmail.com>
2021-03-25 02:55:42 -0400

    Added base utility classes

--- src/main/java/me/firewolf8385/flamepractice/FlamePractice.java
+++ src/main/java/me/firewolf8385/flamepractice/FlamePractice.java
@@ -1,5 +1,8 @@
 package me.firewolf8385.flamepractice;
 
+import me.firewolf8385.flamepractice.utils.gui.GUIListeners;
+import me.firewolf8385.flamepractice.utils.scoreboard.ScoreboardUpdate;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class FlamePractice extends JavaPlugin {
@@ -7,7 +10,8 @@ public final class FlamePractice extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        Bukkit.getPluginManager().registerEvents(new GUIListeners(), this);
+        new ScoreboardUpdate().runTaskTimer(this, 20L, 20L);
     }
 
     @Override
